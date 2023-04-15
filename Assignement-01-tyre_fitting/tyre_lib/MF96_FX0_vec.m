f01335c058800fe24afd46d13a57abe29cbe3553
massimilianopranzo <massimiliano.pranzo@studenti.unitn.it>
2023-04-15 18:13:35 +0200

    Start to work on lateral force fitting @nicobotol

--- Assignement-01-tyre_fitting/tyre_lib/MF96_FX0_vec.m
+++ Assignement-01-tyre_fitting/tyre_lib/MF96_FX0_vec.m
@@ -1,7 +1,7 @@
 % Pure longitudinal force FX0
 % this function remap the sclar function to its vectorial form
 function [fx0_vec] = MF96_FX0_vec(kappa_vec, alpha_vec, phi_vec, Fz_vec, tyre_data)
-
+  % alpha has not to be used
   
   fx0_vec = zeros(size(kappa_vec));
   for i = 1:length(kappa_vec)
